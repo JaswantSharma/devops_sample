6feaaad8d71e920da6bbe489370f04846db40e23
JaswantSharma <droidude1@gmail.com>
2024-10-03 10:59:46 +0000

    devsecops changes 11

--- BuildScripts/buildScript.sh
+++ BuildScripts/buildScript.sh
@@ -6,10 +6,10 @@ set -e  # Exit immediately if a command exits with a non-zero status
    # Update with your actual source path
    # Update with your actual script path
 
-RESULT_FOLDER="$DEST_DIR/result"
+#RESULT_FOLDER="$DEST_DIR/result"
 
 # Step 2: Create the destination directory if it doesn't exist
-sudo mkdir -p "$DEST_DIR"
+#sudo mkdir -p "$DEST_DIR"
 
 # Copy src, test folder, and Makefile to the destination directory
 cp -r "$SOURCE_DIR/src" "$DEST_DIR/src"
@@ -19,20 +19,20 @@ cp "$SOURCE_DIR/Makefile" "$DEST_DIR/Makefile"
 # Step 3: Run the .sh script
 dos2unix "$SH_SCRIPT"
 
-sudo bash "$SH_SCRIPT"
+bash "$SH_SCRIPT"
 
 # Step 4: Create the result directory if it doesn't exist
-sudo mkdir -p "$RESULT_FOLDER"
+#sudo mkdir -p "$RESULT_FOLDER"
 
 # Copy add_me binary and test_add_me.xml into the result folder
-cp "$DEST_DIR/add_me" "$RESULT_FOLDER/add_me"
-cp "$DEST_DIR/test_add_me.xml" "$RESULT_FOLDER/test_add_me.xml"
+cp "$DEST_DIR/add_me" "$SOURCE_DIR"
+cp "$DEST_DIR/test_add_me.xml" "$SOURCE_DIR"
 
 # Step 5: Copy the result folder back to the original source directory
-cp -r "$RESULT_FOLDER" "$SOURCE_DIR/result"
+#cp -r "$RESULT_FOLDER" "$SOURCE_DIR/result"
 
 # Step 6: Clean up - delete the destination directory and its contents
-sudo rm -rf "$DEST_DIR"
+rm -rf "$DEST_DIR"
 
 echo "Script completed successfully."
 
